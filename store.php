15784555d32922aed661d1db7f8f3ff948ea659f
Jason Singh <jasonpoonia12@gmail.com>
2020-04-19 02:32:38 +1200

    Update store.php

--- store.php
+++ store.php
@@ -23,10 +23,12 @@
       background-attachment: scroll;
     }
   }
+
   html {
     scroll-behavior: smooth;
   }
 </style>
+
 <body id="page-top">
   <div class="parallax"></div>
   <!-- Web Design -->
@@ -37,9 +39,10 @@
           <h2 class="section-heading text-uppercase">Web Design</h2>
           <p class="">Lucid Media offers website solutions, we design and develop websites using many different platforms ranging from Wix, Wordpress, Shopify & coded from scratch. We can provide our clients with solutions no matter their
             requirements. Contact us to get an exact quote for a personalised solution for your situation.</h3>
-          <br>
+            <br>
         </div>
       </div>
+      <br>
       <div class="row featurette">
         <div class="col-md-7">
           <h4 class="featurette-heading">Platforms: <span class="text-muted">Wix, Shopify, Wordpress & Squarespace</span></h4>
@@ -85,10 +88,10 @@
               <h4 class="my-0 font-weight-normal">Standard</h4>
             </div>
             <div class="card-body">
-              <h1 class="card-title pricing-card-title">$25 <small class="text-muted">/ week</small></h1>
+              <h1 class="card-title pricing-card-title">$19 <small class="text-muted">/ week</small></h1>
               <ul class="list-unstyled mt-3 mb-4">
                 <li>Instagram & Facebook</li>
-                <li>3 Posts Weekly</li>
+                <li>4 Posts Weekly</li>
                 <li>Hashtag Analysis</li>
                 <li>Ad Campaigns</li>
                 <br>
@@ -98,15 +101,16 @@
           </div>
           <div class="card mb-4 shadow-sm">
             <div class="card-header">
-              <h4 class="my-0 font-weight-normal">Premium</h4>
+              <h4 class="my-0 font-weight-normal">Advanced</h4>
             </div>
             <div class="card-body">
-              <h1 class="card-title pricing-card-title">$40 <small class="text-muted">/ week</small></h1>
+              <h1 class="card-title pricing-card-title">$39 <small class="text-muted">/ week</small></h1>
               <ul class="list-unstyled mt-3 mb-4">
                 <li>Instagram & Facebook</li>
                 <li>5 Posts Weekly</li>
                 <li>Hashtag Analysis</li>
                 <li>Ad Campaigns</li>
+                <li>Content Creation</li>
                 <br>
               </ul>
               <a href="#contact"> <button type="button" class="btn btn-lg btn-block btn-primary">Contact Us</button></a>
@@ -117,7 +121,7 @@
               <h4 class="my-0 font-weight-normal">Enterprise</h4>
             </div>
             <div class="card-body">
-              <h1 class="card-title pricing-card-title">$60 <small class="text-muted">/ week</small></h1>
+              <h1 class="card-title pricing-card-title">$59 <small class="text-muted">/ week</small></h1>
               <ul class="list-unstyled mt-3 mb-4">
                 <li>Instagram & Facebook</li>
                 <li>7 Posts Weekly</li>
@@ -139,28 +143,70 @@
           <h2 class="section-heading text-uppercase">SEO</h2>
           <p class="">Lucid Media offers website solutions, we design and develop websites using many different platforms ranging from Wix, Wordpress, Shopify & coded from scratch. We can provide our clients with solutions no matter their
             requirements. Contact us to get an exact quote for a personalised solution for your situation.</h3>
-          <br>
+            <br>
         </div>
       </div>
+      <br>
       <div class="row featurette">
         <div class="col-md-7">
-          <h4 class="featurette-heading">Platforms: <span class="text-muted">Wix, Shopify, Wordpress & Squarespace</span></h4>
-          <p>Lucid Media offers web design & development on many different platforms. We can give you recommendations depending on a variety of factors, such as Cost, Usage, & Scalability, Customizability. Depending on the following
-            factors our web developers will provide you with an accurate recommendation on which platform we develop your site on.</p>
+          <h4 class="featurette-heading">Packages: <span class="text-muted">Basic, Advanced, Enterprise</span></h4>
+          <p>Lucid Media offers different levels of SEO packages. Basic package provides your website with general optimisation of pages and keywords to ensure your website is appearing in search engine results. Advanced packages would provide
+            everything in Basic, more in-depth keyword research and sitemap creation. Enterprise package would include everything in advanced, Google Search Console, Google Business Setup & more depending on business type.</p>
         </div>
         <div class="col-md-5">
           <img src="img/about/6.png" width="500" height="500" class="bd-placeholder-img-lg featurette-image img-fluid mx-auto" />
         </div>
       </div>
-      <hr class="featurette-divider">
-      <div class="row featurette">
-        <div class="col-md-5">
-          <img src="img/about/5.jpg" width="500" height="500" class="bd-placeholder-img-lg featurette-image img-fluid mx-auto" />
+    </div>
+    <br>
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Standard</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$59 <small class="text-muted"></small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Page Title Optimised</li>
+              <li>Page Description/Keywords</li>
+              <li>Canonical Url</li>
+              <li> </li>
+              <br>
+            </ul>
+            <a href="#contact"> <button type="button" class="btn btn-lg btn-block btn-primary">Contact Us</button></a>
+          </div>
         </div>
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Custom: <span class="text-muted">Developed from scratch</span></h2>
-          <p>If platforms such as Wix, Shopify, Wordpress & Squarespace are not sufficient at meeting your specifications, our developers can provide you with a solution, which is to design & develop your website from scratch. This would
-            mean our developers would plan out your website and code it using HTML, CSS, PHP & Javascript. By developing a website from scratch it means you have control over how the website turns out from the colours, layout & content.</p>
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Advanced</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$99 <small class="text-muted"></small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Everything in Basic</li>
+              <li>In-depth Keyword Research</li>
+              <li>Sitemap Creation</li>
+              <li></li>
+              <br>
+            </ul>
+            <a href="#contact"> <button type="button" class="btn btn-lg btn-block btn-primary">Contact Us</button></a>
+          </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Enterprise</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$139 <small class="text-muted"></small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Everything in Advanced</li>
+              <li>Google Search Console</li>
+              <li>Google Business Setup</li>
+              <li>Google Adwords Campaign</li>
+            </ul>
+            <a href="#contact"> <button type="button" class="btn btn-lg btn-block btn-primary">Contact Us</button></a>
+          </div>
         </div>
       </div>
     </div>
